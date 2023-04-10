285a5a0d825abb8e925323f2c307f0d1de346020
Emathry Chowdhury <emathrychowdhury707@gmail.com>
2023-04-10 15:24:27 +0600

    latest product

--- resources/views/frontendTheme/index.blade.php
+++ resources/views/frontendTheme/index.blade.php
@@ -1,2 +1,22 @@
 <x-frontendTheme.theme_master>
+
+    <div class="container">
+
+        <!--MAIN SLIDE-->
+        <x-frontendTheme.home.slider/>
+
+        <!--BANNER-->
+        <x-frontendTheme.home.banner/>
+
+        <!--On Sale-->
+        <x-frontendTheme.home.on-sale/>
+
+        <!--Latest Products-->
+        <x-frontendTheme.home.latest-product/>
+
+        <!--Product Categories-->
+        <x-frontendTheme.home.product-categories/>
+
+    </div>
+
 </x-frontendTheme.theme_master>
